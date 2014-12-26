9fe8e00a1bbc5eb625bb61cdfdd1e9cc01171791
Ap0c <Ap0c@users.noreply.github.com>
2014-12-26 22:14:09 +0000

    Updated design for mobile users.

--- jeff_control/www/js/navbar.js
+++ jeff_control/www/js/navbar.js
@@ -1,4 +1,19 @@
 // Loads the navbar into pages.
 $(function() {
 	$('#navbar').load('navbar.html');
+	$('#menu_content').load('menu_content.html');
 });
+
+// Produces the drop down menu.
+function drop_menu() {
+	$('#menu_content').css('visibility', 'visible');
+	$('#background').css('visibility', 'visible');
+	$('#menu_wrapper').css('background', 'white');
+};
+
+// Closes the drop down menu.
+function close_menu() {
+	$('#menu_content').css('visibility', 'hidden');
+	$('#background').css('visibility', 'hidden');
+	$('#menu_wrapper').css('background', '#5CB8E6');
+};
