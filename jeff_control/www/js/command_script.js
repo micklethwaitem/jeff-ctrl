4afacd1eff2f033396d61565de8e1bfd0f876893
micklethwaitem <micklethwaitem@hotmail.com>
2015-01-22 18:01:32 +0000

    Clean&Comment
    Cleaned and commented a bit

--- jeff_control/www/js/command_script.js
+++ jeff_control/www/js/command_script.js
@@ -7,82 +7,82 @@ var command_list = [];
 // Allow input of number for a command
 function add_varnum(button) {
 
-	// Activates number input for functions
+	// Activates number input
 	if (button == 'forward' || button == 'left' || button == 'right' || button == 'backward') {
-      document.getElementById('time_select').className = "number_input_active";
-      document.getElementById('loop_number_select').className = "number_input_inactive";
-   }
-   else if (button == 'for') {
-      document.getElementById('time_select').className = "number_input_inactive";
-      document.getElementById('loop_number_select').className = "number_input_active";
-   }
+		document.getElementById('time_select').className = "number_input_active";
+		document.getElementById('loop_number_select').className = "number_input_inactive";
+	}
+	else if (button == 'for') {
+		document.getElementById('time_select').className = "number_input_inactive";
+		document.getElementById('loop_number_select').className = "number_input_active";
+	}
 
    	// Deactivates any active button
-   var active = document.getElementsByClassName('button_active');
-   for( var i = 0; i < active.length; i++) {
-      active[i].className = "button_inactive";
-   }
+	var active = document.getElementsByClassName('button_active');
+	for( var i = 0; i < active.length; i++) {
+		active[i].className = "button_inactive";
+	}
    
    	// Activates button clicked
-   if (button == 'forward') {
-      document.getElementById('forward_button').className = "button_active";
-   }
-   else if (button == 'left') {
-      document.getElementById('left_button').className = "button_active";
-   }
-   else if (button == 'right') {
-      document.getElementById('right_button').className = "button_active";
-   }
-   else if (button == 'backward') {
-      document.getElementById('backward_button').className = "button_active";
-   }
-   else if (button == 'for') {
-      document.getElementById('for_button').className = "button_active";
-   }
+	if (button == 'forward') {
+		document.getElementById('forward_button').className = "button_active";
+	}
+	else if (button == 'left') {
+		document.getElementById('left_button').className = "button_active";
+	}
+	else if (button == 'right') {
+		document.getElementById('right_button').className = "button_active";
+	}
+	else if (button == 'backward') {
+		document.getElementById('backward_button').className = "button_active";
+	}
+	else if (button == 'for') {
+		document.getElementById('for_button').className = "button_active";
+	}
 
 }
 
 
 function add_div() {
-   
-   var amount = document.getElementsByClassName('number_input_active')[0].getElementsByClassName('div_amt')[0].value;
-   var command_raw = document.getElementsByClassName('button_active');
-   var command;
-   
-   /* Safari is bad */
-   if(amount == "" || amount < 0 || amount > 100) {
-      return false;
-   }
-   
-   
-   
-   switch (command_raw[0].id) {
-      case "forward_button":
-         command = "forward";
-         break;
-      case "backward_button":
-         command = "backward";
-         break;
-      case "left_button":
-         command = "left";
-         break;
-      case "right_button":
-         command = "right";
-         break;
-      case "for_button":
-         if(amount % 1 != 0 || amount < 2) {
-			 return false;
-		 }
-         command = "for";
-         break;
+	
+	var amount = document.getElementsByClassName('number_input_active')[0].getElementsByClassName('div_amt')[0].value;
+	var command_raw = document.getElementsByClassName('button_active');
+	var command;
+	
+	/* Safari is incompatible with HTML5 form validation. This line to ensure entry of a valid number. */
+	if(amount == "" || amount < 0 || amount > 100) {
+		return false;
+	}
+
+	switch (command_raw[0].id) {
+		case "forward_button":
+			command = "forward";
+			break;
+		case "backward_button":
+			command = "backward";
+			break;
+		case "left_button":
+			command = "left";
+			break;
+		case "right_button":
+			command = "right";
+			break;
+		case "for_button":
+			/* Safari is incompatible with HTML5 form validation. This line to ensure integer > 2 input for # loops. */
+			if(amount % 1 != 0 || amount < 2) {
+				return false;
+			}
+			command = "for";
+			break;
 		default:
 			window.alert("Cannot find command type?");
-         break;
-   }
+		break;
+	}
    
-   move(command, amount);
+	move(command, amount);
    
-   return false;
+	return false;
+	
 }
 
 
@@ -98,35 +98,34 @@ function move(command, amount) {
 		new_item_display = new_item[i].getElementsByClassName('display_cmd');
 		new_item_cmd = new_item[i].getElementsByClassName('hidden_cmd');
 		new_item_amount = new_item[i].getElementsByClassName('hidden_amount');
-      
-      if(command == 'forward' || command == 'backward' || command == 'left' || command == 'right') {
-         loop_contents = new_item[i].getElementsByClassName('loop_contents_active');
-         loop_end = new_item[i].getElementsByClassName('loop_end_active');
-   
-         new_item_display[0].innerHTML = command + '(' + amount + ');';
-         new_item_cmd[0].innerHTML = command;
-         new_item_amount[0].innerHTML = amount;
-         if(loop_end.length) {         // also loop_contents
-            loop_contents[0].className = 'loop_contents_inactive';
-            loop_end[0].className = 'loop_end_inactive';
-         }
-      }
-      else if(command == 'for') {
-         loop_contents = new_item[i].getElementsByClassName('loop_contents_inactive');
-         loop_end = new_item[i].getElementsByClassName('loop_end_inactive');
-      
-         new_item_display[0].innerHTML = 'loop from 1 to ' + amount + ':';
-         new_item_cmd[0].innerHTML = command;
-         new_item_amount[0].innerHTML = amount;
-         if(loop_end.length) {       // also loop_contents
-            loop_contents[0].className = 'loop_contents_active';
-            loop_end[0].className = 'loop_end_active';
-         }
-      }
-	}	/* This loop is annoying but necessary. Dragging always creates a clone, and if the dragged object is not
-		dropped into the list, it returns. This overlaps with previous, and so all must be changed on button click.
-		It is a bug in the library code. There is likely a simple, cleaner alternative (deleting the dragged
-		element if not dropped into list), but this works for now. */
+
+		if(command == 'forward' || command == 'backward' || command == 'left' || command == 'right') {
+			loop_contents = new_item[i].getElementsByClassName('loop_contents_active');
+			loop_end = new_item[i].getElementsByClassName('loop_end_active');
+
+			new_item_display[0].innerHTML = command + '(' + amount + ');';
+			new_item_cmd[0].innerHTML = command;
+			new_item_amount[0].innerHTML = amount;
+			if(loop_end.length) {         // also loop_contents
+				loop_contents[0].className = 'loop_contents_inactive';
+				loop_end[0].className = 'loop_end_inactive';
+			}
+		}
+		
+		else if(command == 'for') {
+			loop_contents = new_item[i].getElementsByClassName('loop_contents_inactive');
+			loop_end = new_item[i].getElementsByClassName('loop_end_inactive');
+
+			new_item_display[0].innerHTML = 'loop from 1 to ' + amount + ':';
+			new_item_cmd[0].innerHTML = command;
+			new_item_amount[0].innerHTML = amount;
+			if(loop_end.length) {       // also loop_contents
+				loop_contents[0].className = 'loop_contents_active';
+				loop_end[0].className = 'loop_end_active';
+			}
+		}
+	}	/* These loops is annoying but necessary. Dragging always creates a clone, and if the dragged object is not
+		dropped into the list, it returns. This overlaps with previous, and so all must be changed on button click. */
 
 }
 
@@ -175,10 +174,10 @@ function run_commands() {
 
 	/* Create array from list */
 	var list = document.getElementById("command_list").getElementsByClassName("list_item");
-   var length = list.length;
+	var length = list.length;
 	create_array(list, 0, length);        // List of divs; Start position; End position (for recursive)
 
-   
+
 /*		[-------------  TESTING PURPOSES ONLY  -------------] 
 		Alert array contents: */
 /*	window.alert(command_list);			*/
@@ -194,7 +193,7 @@ function run_commands() {
 
 		handle_command(command, duration);
 
-	};
+	}
 }
 
 // Creates array using ordered list of divs
@@ -203,46 +202,48 @@ function create_array(list, start, end) {
 	var command, amount;
 	
 	for(var i = start; i < end; i++) {
-      
+
 		command = list[i].getElementsByClassName('hidden_cmd')[0].innerHTML;
 		amount = list[i].getElementsByClassName('hidden_amount')[0].innerHTML;
       
-      if(command == "for") {
+		if(command == "for") {
          
-         loop_content = list[i].getElementsByClassName("list_item");
-         var length = loop_content.length;
-         
-         for(var j = 0; j < amount; j++) {
-            create_array(list, i + 1, i + length + 1);
-         }
-         
-         i = i + length;
-      }
+			loop_content = list[i].getElementsByClassName("list_item");
+			var length = loop_content.length;
+			
+			// Recursive for loops
+			for(var j = 0; j < amount; j++) {
+				create_array(list, i + 1, i + length + 1);
+			}
+
+			i = i + length;
+		}
       
-      else {
-         command_list.push([command, amount]);
-      }
-      
+		else {
+			command_list.push([command, amount]);
+		}
+
 	};
 	
 }
 
 // Add for loop to array
 function create_array_loop(list, start, amount) {
+
+	loop_content = list[start].getElementsByClassName("list_item");
+	var length = loop_content.length;
+	var num_loops = list[start].getElementsByClassName('hidden_amount')[0].innerHTML;
    
-   loop_content = list[start].getElementsByClassName("list_item");
-   var length = loop_content.length;
-   var num_loops = list[start].getElementsByClassName('hidden_amount')[0].innerHTML;
+	for(var j = 0; j < num_loops; j++) {
+		for(var k = 0; k < length; k++) {
+			command = list[k+start+1].getElementsByClassName('hidden_cmd')[0].innerHTML;
+			amount = list[k+start+1].getElementsByClassName('hidden_amount')[0].innerHTML;
+			command_list.push([command, amount]);
+		}
+	}
    
-   for(var j = 0; j < num_loops; j++) {
-      for(var k = 0; k < length; k++) {
-         command = list[k+start+1].getElementsByClassName('hidden_cmd')[0].innerHTML;
-         amount = list[k+start+1].getElementsByClassName('hidden_amount')[0].innerHTML;
-         command_list.push([command, amount]);
-      }
-   }
-   
-   return start + length;
+	return start + length;
+	
 }
 
 /* ----- Page Styling ----- */
@@ -253,31 +254,35 @@ function activate_tab(tab) {
 	// Activates tab for functions.
 	if (tab == 'functions_tab') {
 
-      document.getElementById('control_variables').className = "control_inactive";
+		document.getElementById('control_variables').className = "control_inactive";
 		document.getElementById('variables_tab').className = "tab_inactive";
-      document.getElementById('control_loops').className = "control_inactive";
+		document.getElementById('control_loops').className = "control_inactive";
 		document.getElementById('loops_tab').className = "tab_inactive";
-      document.getElementById('control_functions').className = "control_active";
+		document.getElementById('control_functions').className = "control_active";
 		document.getElementById('functions_tab').className = "tab_active";
 
-	// Activates tab for variables.
-	} else if (tab == 'variables_tab') {
+	}
 
-      document.getElementById('control_variables').className = "control_active";
+	// Activates tab for variables.
+	else if (tab == 'variables_tab') {
+
+		document.getElementById('control_variables').className = "control_active";
 		document.getElementById('variables_tab').className = "tab_active";
-      document.getElementById('control_loops').className = "control_inactive";
+		document.getElementById('control_loops').className = "control_inactive";
 		document.getElementById('loops_tab').className = "tab_inactive";
-      document.getElementById('control_functions').className = "control_inactive";
+		document.getElementById('control_functions').className = "control_inactive";
 		document.getElementById('functions_tab').className = "tab_inactive";
 
+	}
+	
 	// Activates tab for loops.
-	} else if (tab == 'loops_tab') {
+	else if (tab == 'loops_tab') {
 
-      document.getElementById('control_variables').className = "control_inactive";
+		document.getElementById('control_variables').className = "control_inactive";
 		document.getElementById('variables_tab').className = "tab_inactive";
-      document.getElementById('control_loops').className = "control_active";
+		document.getElementById('control_loops').className = "control_active";
 		document.getElementById('loops_tab').className = "tab_active";
-      document.getElementById('control_functions').className = "control_inactive";
+		document.getElementById('control_functions').className = "control_inactive";
 		document.getElementById('functions_tab').className = "tab_inactive";
 
 	}
--- jeff_control/www/js/drag_drop.js
+++ jeff_control/www/js/drag_drop.js
@@ -6,32 +6,33 @@ Sortable.create(list_commands, {
 		name: 'sorting',
 		put: ["newitem"]					// Add to list from newitem
 	},
-	handle: '.handle',
+	handle: '.handle',						// Draggable part
 	animation: 150,							// Movement speed when rearranging
 	sort: true,								// Can reorder within list
-   scroll: true,
-   scrollSensitivity: 80,
-   scrollSpeed: 10,
+	scroll: true,
+	scrollSensitivity: 80,					// Scrolls when near edge of page
+	scrollSpeed: 10,
    
    
 	/* UPON ADDITION TO LIST.
 	Display delete button [must not delete original list item].*/
-    onAdd: function (evt) {					// When new element is dropped into list
+    onAdd: function (evt) {
 		var del = evt.item.getElementsByClassName("delete_button");
 		del[0].style.display = "block";
-      var nested_list = evt.item.getElementsByClassName("loop_contents_active");
-      if(nested_list.length) {               // If addition is a loop
-         Sortable.create(nested_list[0], {   // Create new, nested list within loop
-            group: {
-               name: 'sorting',
-               put: ["newitem"]					// Add to list from newitem
-            },
-            handle: '.handle',
-            animation: 150,
-            sort: true,
-         });
-      };
-    },
+		
+		var nested_list = evt.item.getElementsByClassName("loop_contents_active");
+		if(nested_list.length) {               // If addition is a loop
+			Sortable.create(nested_list[0], {   // Create new, nested list within loop
+				group: {
+					name: 'sorting',
+					put: ["newitem"]
+				},
+				handle: '.handle',
+				animation: 150,
+				sort: true,
+			});
+		};
+	},
 });
 
 // Drag and Drop New Item
@@ -45,7 +46,7 @@ Sortable.create(new_commands, {
 	animation: 500,
 });
 
-
+// Delete list element (parent of delete button is passed to this function)
 function delete_item(marked_delete) {
 	marked_delete.parentNode.removeChild(marked_delete);
 }
